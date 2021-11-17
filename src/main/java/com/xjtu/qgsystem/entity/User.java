81fee145e09534bdf3396cea4ed54d3a4a856315
congregalis <2056843452@qq.com>
2021-11-18 01:30:50 +0800

    初步完成用户登陆功能

---
+++ src/main/java/com/xjtu/qgsystem/entity/User.java
@@ -0,0 +1,67 @@
+package com.xjtu.qgsystem.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String avatar;
+    private String introduction;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+}
--- src/main/java/com/xjtu/qgsystem/service/UserService.java
+++ src/main/java/com/xjtu/qgsystem/service/UserService.java
@@ -1,32 +1,63 @@
 package com.xjtu.qgsystem.service;
 
+import cn.hutool.jwt.JWT;
+import cn.hutool.jwt.JWTUtil;
+import com.xjtu.qgsystem.entity.User;
+import com.xjtu.qgsystem.repository.UserRepository;
 import com.xjtu.qgsystem.util.result.Result;
 import com.xjtu.qgsystem.util.result.ResultUtil;
 import com.xjtu.qgsystem.vo.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
+
+import cn.hutool.*;
 
 @Service
 public class UserService {
+    private final String secret = "QG_SYSTEM_SERVER";
+
+    @Autowired
+    private UserRepository userRepository;
 
     public String login(String username, String password) {
-        if (username.equals("admin") && password.equals("zsclqg")) {
-            return "admin-token";
+        Optional<User> byUsername = userRepository.findByUsername(username);
+        if (!byUsername.isPresent()) return "-1";
+
+        User user = byUsername.get();
+
+        // todo: 密码暂时先用明文存在数据库中，后续更换更安全待方式
+        if (user.getPassword().equals(password)) {
+            String introduction = user.getIntroduction();
+            String avatar = user.getAvatar();
+            String name = user.getName();
+
+            Map<String, Object> userMap = new HashMap<String, Object>() {
+                private static final long serialVersionUID = 1L;
+                {
+                    put("name", name);
+                    put("introduction", introduction);
+                    put("avatar", avatar);
+                }
+            };
+            return JWTUtil.createToken(userMap, secret.getBytes());
         } else {
             return "-1";
         }
     }
 
     public UserVO getInfo(String token) {
-        if (token.equals("admin-token")) {
+        if (JWTUtil.verify(token, secret.getBytes())) {
+            final JWT jwt = JWTUtil.parseToken(token);
+            // todo: 权限组功能暂未做
             return new UserVO(Collections.singletonList("admin"),
-                    "I am a super administrator",
-//                    "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
-                    "https://img.zcool.cn/community/01e8fa5965991ba8012193a3195e5a.gif",
-                    "Super Admin");
-        } else return null;
+                    (String) jwt.getPayload("introduction"),
+                    (String) jwt.getPayload("avatar"),
+                    (String) jwt.getPayload("name"));
+        } else {
+            return null;
+        }
     }
 
     public String logout() {
