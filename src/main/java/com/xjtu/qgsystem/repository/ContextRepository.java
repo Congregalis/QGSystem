f60d7cc85fe54a60204fe42f3e4c47295427d337
zzmqaq <7661663+zzmqaq@user.noreply.gitee.com>
2022-05-25 10:31:50 +0800

    新增根据筛选条件获取上下文接口

--- src/main/java/com/xjtu/qgsystem/repository/ContextRepository.java
+++ src/main/java/com/xjtu/qgsystem/repository/ContextRepository.java
@@ -1,7 +1,10 @@
 package com.xjtu.qgsystem.repository;
 
 import com.xjtu.qgsystem.entity.Context;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +14,8 @@ public interface ContextRepository extends JpaRepository<Context, Long> {
     public Optional<Context> findById(Long id);
 
     public List<Context> findByTitle(String title);
+
+    public Page<Context> findBySubjectAndLanguage(String subject, String language, Pageable pageable);
+
 
 }
